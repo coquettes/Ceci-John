35981c6dcc7e41933672df8fbd7e82c803874c51
coquettes <161481325+coquettes@users.noreply.github.com>
2024-10-10 11:47:30 -0300

    ~çç~ç

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+    'História',
+    'Quais foram os estados que realizaram a política do Café com Leite?',
+    'São Paulo e Minas Gerais')
+    
+    criaCartao(
+    'História',
+    'Quem foi o 1° presidente do Brasil?',
+    'Deodoro da Fonseca')
+    
+    criaCartao(
+    'História',
+    'Causa da morte de Getúlio Vargas?',
+    'Suicídio')
+    
+    criaCartao(
+    'História',
+    'Em que ano foi realizado o golpe militar no Brasil?',
+    '1964')
+    
+    criaCartao(
+    'História',
+    'Qual era o apelido do político João Goulart?',
+    'Jango')
+    
+    criaCartao(
+    'História',
+    'Em qual país iniciou a Revolução Industrial?',
+    'Inglaterra')
+    
+    criaCartao(
+    'História',
+    'Em que ano foi declarada a Independência do Brasil?',
+    '1822')
+    
+    criaCartao(
+    'História',
+    'Qual foi o país que sediou as Olimpíadas de 2016?',
+    'Brasil')
+    
+    criaCartao(
+    'História',
+    'Qual era o político que dizia que iria varrer a corrupção?',
+    'Jânio Quadros')
+    
+    criaCartao(
+    'História',
+    'Como é chamado o dia da invasão da Normandia na Segunda Guerra Mundial?',
+    'Dia D')
+    
+    criaCartao(
+    'História',
+    'Quem foi líder do regime totalitário nazista?',
+    'Adolf Hitler')
+    
+    criaCartao(
+    'História',
+    'Quais estados disputaram a região denominada Contestado?',
+    'São Paulo e Santa Catarina')
+    
+    criaCartao(
+    'História',
+    'Qual revolução é liderada pela oligarquia paulista que exigia uma nova constituição?',
+    'Constitucionalista')
+    
+    criaCartao(
+    'História',
+    'Quem assumiu o governo em 1946, após a saída de Vargas?',
+    'Dutra')
+    
+    criaCartao(
+    'História',
+    'Qual empresa foi criada no Brasil em 1953, após grande campanha governamental?',
+    'Petrobras')
+    
+    criaCartao(
+    'História',
+    'Quais foram os aliados de Vargas?',
+    'Elite industrial e operário')
+    
+    criaCartao(
+    'História',
+    'Qual político tinha como lema realizar 50 anos em 5?',
+    'Jucelino Kubitschek')
+    
+    criaCartao(
+    'História',
+    'Durante a Guerra Fria, o mundo foi dividido em dois blocos, quais?',
+    'Capitalista e socialista')
+    
+    criaCartao(
+    'História',
+    'Em que ano iniciou a revolta de canudos?',
+    '1893')
+    
+    criaCartao(
+    'História',
+    'Qual o significado da sigla CLT?',
+    'consolidação das leis do trabalho')
